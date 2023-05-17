288ae4b9006b08bb72db82094988dacbfd20b8f3
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-05-17 13:42:27 +0200

    Revert "update"
    This reverts commit 6bdfb68fe3b27fd0f7fe0e0ed74c095f93ecaf12.

--- src/redux/books/booksAction.js
+++
@@ -1,7 +0,0 @@
- import {createAction} from '@reduxjs/toolkit'
-
- export const fetchBooksRequest = createAction('books/fetchBooksRequest');
-
- export const fetchBooksSuccess = createAction('books/fetchBooksSuccess');
-
- export const fetchBooksError = createAction('books/fetchBooksError')
--- src/src/redux/books/booksSelectors.js
+++
@@ -1,2 +0,0 @@
-export const getBooks = state => state.books.entities;
-export const getBookInfo = state => state.books.info;
